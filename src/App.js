483baa565dfc8e9aae01917cb524f3d7d5b026f0
Awais <awais.khalid@gosign.de>
2020-03-06 16:18:20 +0500

    add page not found route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ShipmentList from './components/ShipmentList';
 import ShipmentDetail from './components/ShipmentDetail';
+import Pagenotfound from './components/Pagenotfound';
 import { Route, Switch } from "react-router-dom";
 import "./App.scss";
 
@@ -11,6 +12,7 @@ class App extends Component {
 				<Switch>
 					<Route path="/" exact component={ShipmentList}></Route>
 					<Route path="/shipment/:id" component={ShipmentDetail}></Route>
+					<Route path="*" component={Pagenotfound} />
 				</Switch>
 			</div>
 		);
---
+++ src/components/Pagenotfound.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Pagenotfound = () => {
+    return (
+        <div>Page Not Found</div>
+    );
+}
+
+export default Pagenotfound;
